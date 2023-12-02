88aa7ae16956bfd659d2f4a2e72ceeca775d751d
Mostrosalvado <diealesal@gmail.com>
2023-12-02 08:58:50 -0300

    funcionan todo, mejorar icono de carrito y sumar productos

--- src/context/Reducer.js
+++ src/context/Reducer.js
@@ -1,36 +1,38 @@
+/* eslint-disable default-case */
 //TYPES
 
+const LISTAME_PRODUCTOS = "LISTAME_PRODUCTOS";
+const AGREGAR_CARRITO = "AGREGAR_CARRITO";
+const ELIMINAR_CARRITO = "ELIMINAR_CARRITO";
 
-const LISTAME_PRODUCTOS = "LISTAME_PRODUCTOS"
-const AGREGAR_CARRITO = "AGREGAR_CARRITO"
-const ELIMINAR_CARRITO = "ELIMINAR_CARRITO"
-
-export default function Reducer(state, action){
-    const {payload , type } = action;
-    switch(type){
+export default function Reducer(state, action) {
+    const { payload, type } = action;
+    switch (type) {
         case LISTAME_PRODUCTOS:
-            console.log(payload)
-            return {...state, productos:payload};
-
+            console.log(payload);
+            return { ...state, productos: payload };
         case AGREGAR_CARRITO:
             return {
                 ...state,
                 carrito: [
                     ...state.carrito,
-                    state.productos.filter((ite)=> ite.id === parseInt (payload)),
-                ]
-            }
-            case ELIMINAR_CARRITO:
-                return {
-                    ...state,
-                    carrito: [
-                        ...state.carrito,
-                        state.carrito.filter((ite)=>ite[0].id !== parseInt(payload)),
-                    ]
-                }
+                    state.productos.filter((ite) => ite.id === parseInt(payload)),
+                ],
+            };
 
+        case ELIMINAR_CARRITO:
+            console.log(
+                payload,
+                "<- payload, state ->",
+                state.carrito,
+                "lo que llega"
+            );
+            return {
+                ...state,
+                carrito: state.carrito.filter((items) => items[0].id !== payload),
+            };
+
+        default:
+            console.log("default")
     }
-    
-
 }
-
